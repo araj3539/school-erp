babf50d6ad8ce051648214a791aa1bd7a76dbcd6
Alok Raj <13000122091.tmsl@ticollege.org>
2026-08-18 21:05:29 +0530

    refactor(storage): remove Cloudinary configuration

--- server/src/config/cloudinary.ts
+++ server/src/config/cloudinary.ts
@@ -1,2 +1,1 @@
-import { v2 as cloudinary } from "cloudinary";import { env } from "./env.js";if (env.CLOUDINARY_CLOUD_NAME && env.CLOUDINARY_API_KEY && env.CLOUDINARY_API_SECRET) {  cloudinary.config({    cloud_name: env.CLOUDINARY_CLOUD_NAME,    api_key: env.CLOUDINARY_API_KEY,    api_secret: env.CLOUDINARY_API_SECRET,    secure: true  });}export { cloudinary };export const isCloudinaryConfigured = Boolean(env.CLOUDINARY_CLOUD_NAME && env.CLOUDINARY_API_KEY && env.CLOUDINARY_API_SECRET);
-
+// Cloudinary has been removed. File storage is handled by Cloudflare R2.
