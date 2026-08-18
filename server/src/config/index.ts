6711e4cab33378519ae2076b0a6dcf204841690f
Alok Raj <13000122091.tmsl@ticollege.org>
2026-08-18 21:58:51 +0530

    refactor(storage): remove obsolete Cloudinary config export

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,2 +1,3 @@
-export * from "./env.js";export * from "./db.js";export * from "./cloudinary.js";export * from "./constants.js";
-
+export * from "./env.js";
+export * from "./db.js";
+export * from "./constants.js";
