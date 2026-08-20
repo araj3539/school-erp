39cd2f491912c4299e26322f50af6a9a5dd7f49a
Alok Raj <13000122091.tmsl@ticollege.org>
2026-08-20 12:14:09 +0530

    Add student document recovery route

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -6,6 +6,7 @@ import LoginPage from "../pages/LoginPage";
 import DashboardPage from "../pages/DashboardPage";
 import StudentsPage from "../pages/StudentsPage";
 import StudentDetailPage from "../pages/StudentDetailPage";
+import StudentDocumentRecoveryPage from "../pages/StudentDocumentRecoveryPage";
 import TeachersPage from "../pages/TeachersPage";
 import ClassesPage from "../pages/ClassesPage";
 import AttendancePage from "../pages/AttendancePage";
@@ -14,12 +15,7 @@ import ReportsPage from "../pages/ReportsPage";
 import SettingsPage from "../pages/SettingsPage";
 
 export const router = createBrowserRouter([
-  {
-    element: <AuthLayout />,
-    children: [
-      { path: "/login", element: <LoginPage /> }
-    ]
-  },
+  { element: <AuthLayout />, children: [{ path: "/login", element: <LoginPage /> }] },
   {
     element: <RequireAuth><AdminLayout /></RequireAuth>,
     children: [
@@ -27,6 +23,7 @@ export const router = createBrowserRouter([
       { path: "/dashboard", element: <DashboardPage /> },
       { path: "/students", element: <StudentsPage /> },
       { path: "/students/:id", element: <StudentDetailPage /> },
+      { path: "/students/:id/document-recovery", element: <StudentDocumentRecoveryPage /> },
       { path: "/teachers", element: <TeachersPage /> },
       { path: "/classes", element: <ClassesPage /> },
       { path: "/attendance", element: <AttendancePage /> },
