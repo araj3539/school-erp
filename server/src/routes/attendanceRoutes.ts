e8fd744b47581277d8fa08412855f7c733cb005c
Alok Raj <13000122091.tmsl@ticollege.org>
2026-08-21 20:44:19 +0530

    security: allow student own attendance permission path

--- server/src/routes/attendanceRoutes.ts
+++ server/src/routes/attendanceRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { authenticate, requirePermission, validate } from "../middleware/index.js";
+import { authenticate, requirePermission, requireAnyPermission, validate } from "../middleware/index.js";
 import { getAttendance, markAttendance, getStudentAttendance, getMonthlyAttendanceReport } from "../controllers/attendanceController.js";
 import { MarkAttendanceSchema, PaginationSchema, DateRangeSchema, IdParamSchema } from "../validators/index.js";
 
@@ -8,7 +8,7 @@ const router = Router();
 router.use(authenticate);
 router.get("/", requirePermission("attendance:read"), validate(PaginationSchema, "query"), getAttendance);
 router.post("/", requirePermission("attendance:write"), validate(MarkAttendanceSchema), markAttendance);
-router.get("/student/:id", requirePermission("attendance:read"), validate(IdParamSchema, "params"), validate(DateRangeSchema, "query"), getStudentAttendance);
+router.get("/student/:id", requireAnyPermission("attendance:read", "attendance:read:own"), validate(IdParamSchema, "params"), validate(DateRangeSchema, "query"), getStudentAttendance);
 router.get("/report/monthly", requirePermission("attendance:read"), getMonthlyAttendanceReport);
 
 export default router;
