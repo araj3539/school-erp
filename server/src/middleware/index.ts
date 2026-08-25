170d84e7703491c170d17be58e17b3a9f3f299ca
Alok Raj <13000122091.tmsl@ticollege.org>
2026-08-25 13:22:33 +0530

    feat(security): export payment ownership middleware

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -1,2 +1,2 @@
-export * from "./auth.js";export * from "./rbac.js";export * from "./validation.js";export * from "./errorHandler.js";export * from "./rateLimiter.js";export * from "./upload.js";
+export * from "./auth.js";export * from "./rbac.js";export * from "./validation.js";export * from "./errorHandler.js";export * from "./rateLimiter.js";export * from "./upload.js";export * from "./paymentOwnership.js";
 
