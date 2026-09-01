4276205cb17d1114d1d2e68833ea8bbad166ce0e
Alok Raj <13000122091.tmsl@ticollege.org>
2026-09-01 20:09:09 +0530

    fix(teachers): wire validated search filters

--- server/src/routes/teacherRoutes.ts
+++ server/src/routes/teacherRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { authenticate, requirePermission, validate } from "../middleware/index.js";
 import { getTeachers, getTeacherById, createTeacher, updateTeacher, deleteTeacher } from "../controllers/teacherController.js";
-import { CreateTeacherSchema, UpdateTeacherSchema, PaginationSchema, IdParamSchema } from "../validators/index.js";
+import { CreateTeacherSchema, UpdateTeacherSchema, TeacherQuerySchema, IdParamSchema } from "../validators/index.js";
 
 const router = Router();
 
 router.use(authenticate);
-router.get("/", requirePermission("teachers:read"), validate(PaginationSchema, "query"), getTeachers);
+router.get("/", requirePermission("teachers:read"), validate(TeacherQuerySchema, "query"), getTeachers);
 router.get("/:id", requirePermission("teachers:read"), validate(IdParamSchema, "params"), getTeacherById);
 router.post("/", requirePermission("teachers:write"), validate(CreateTeacherSchema), createTeacher);
 router.put("/:id", requirePermission("teachers:write"), validate(IdParamSchema, "params"), validate(UpdateTeacherSchema), updateTeacher);
