1c02c744e7ea5d1af158eabd2a08612504b3f528
Alok Raj <13000122091.tmsl@ticollege.org>
2026-09-03 14:33:10 +0530

    feat(phase6): export homework contracts

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,5 +1,5 @@
 export * from "./schemas/index.js";
+export * from "./schemas/phase6.js";
 export * from "./constants/index.js";
 export * from "./utils/index.js";
 export { UserRole, UserStatus, StudentStatus, TeacherStatus, AttendanceStatus, FeeType, FeeStatus, PaymentMode, Gender, BloodGroup, DocumentType } from "./constants/index.js";
-
