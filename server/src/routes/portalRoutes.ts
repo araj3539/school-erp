a7d3344364f58cc043b55769047361b3adbdad14
Alok Raj <13000122091.tmsl@ticollege.org>
2026-09-03 21:15:58 +0530

    feat(phase7): expose teacher homework portal APIs

--- server/src/routes/portalRoutes.ts
+++ server/src/routes/portalRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { authenticate } from "../middleware/index.js";
+import { authenticate, requirePermission, validate } from "../middleware/index.js";
 import { getPortalDashboard } from "../controllers/portalController.js";
 import { getTeacherWorkspace } from "../controllers/teacherPortalController.js";
+import { getTeacherHomework, getTeacherHomeworkOptions } from "../controllers/teacherHomeworkController.js";
+import { createHomework } from "../controllers/homeworkController.js";
+import { CreateHomeworkSchema } from "../validators/index.js";
 import { UserRole } from "@school-erp/shared";
 
 const router = Router();
@@ -15,5 +18,14 @@ router.get("/dashboard", (req, res, next) => {
 });
 router.get("/teacher/workspace", (req, res, next) => {
   getTeacherWorkspace(req, res, next);
+});
+router.get("/teacher/homework/options", requirePermission("homework:write"), (req, res, next) => {
+  getTeacherHomeworkOptions(req, res, next);
+});
+router.get("/teacher/homework", requirePermission("homework:read"), (req, res, next) => {
+  getTeacherHomework(req, res, next);
+});
+router.post("/teacher/homework", requirePermission("homework:write"), validate(CreateHomeworkSchema), (req, res, next) => {
+  createHomework(req, res, next);
 });
 export default router;
