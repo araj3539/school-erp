c6a5a5625cbfa0abf8d8fa1fbfd5601f9228b13e
Alok Raj <13000122091.tmsl@ticollege.org>
2026-09-07 19:57:43 +0530

    feat(ALO-31): export staff contracts

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./schemas/index.js";
 export * from "./schemas/phase6.js";
 export * from "./schemas/phase9.js";
+export * from "./schemas/phase10.js";
 export * from "./constants/index.js";
 export * from "./utils/index.js";
 export { UserRole, UserStatus, StudentStatus, TeacherStatus, AttendanceStatus, FeeType, FeeStatus, PaymentMode, Gender, BloodGroup, DocumentType } from "./constants/index.js";
