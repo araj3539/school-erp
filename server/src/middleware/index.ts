fc076655762f3eed4c61bdb79808415a45e3ee9e
Alok Raj <13000122091.tmsl@ticollege.org>
2026-09-09 02:01:55 +0530

    chore(saas): export platform authorization middleware

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -1,2 +1,1 @@
 export * from "./auth.js";export * from "./rbac.js";export * from "./validation.js";export * from "./errorHandler.js";export * from "./rateLimiter.js";export * from "./upload.js";export * from "./paymentOwnership.js";
-
