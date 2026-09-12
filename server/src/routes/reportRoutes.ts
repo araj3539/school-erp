b428e71d94c3b8c227e8653fd337ba1b977f6b02
Alok Raj <13000122091.tmsl@ticollege.org>
2026-09-12 17:51:17 +0530

    feat: wire management report exports

--- server/src/routes/reportRoutes.ts
+++ server/src/routes/reportRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authenticate, requirePermission, validate } from "../middleware/index.js";
 import { getAttendanceReport, getFeeReport, getStudentReport } from "../controllers/reportController.js";
+import { exportManagementReport } from "../controllers/reportExportController.js";
 import { ReportQuerySchema } from "../validators/index.js";
 
 const router = Router();
@@ -8,5 +9,6 @@ router.use(authenticate, requirePermission("reports:read"));
 router.get("/students", validate(ReportQuerySchema, "query"), getStudentReport);
 router.get("/attendance", validate(ReportQuerySchema, "query"), getAttendanceReport);
 router.get("/fees", validate(ReportQuerySchema, "query"), getFeeReport);
+router.get("/export", exportManagementReport);
 
 export default router;
