25d770461e7bcd32783580685e98df20b7c5ff78
Alok Raj <13000122091.tmsl@ticollege.org>
2026-09-13 01:09:36 +0530

    feat: export family and fee item services

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -9,3 +9,5 @@ export * from "./domainRules.js";
 export * from "./scheduledJobService.js";
 export * from "./calendarAutomationService.js";
 export * from "./upiPaymentService.js";
+export * from "./studentFamilyService.js";
+export * from "./feeItemService.js";
