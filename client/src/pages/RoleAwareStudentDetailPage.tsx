e12e9372334869e262daf2fcf89e2c61e591359b
Alok Raj <13000122091.tmsl@ticollege.org>
2026-09-13 01:49:51 +0530

    feat: surface student fee breakdown management

--- client/src/pages/RoleAwareStudentDetailPage.tsx
+++ client/src/pages/RoleAwareStudentDetailPage.tsx
@@ -2,6 +2,7 @@ import { useAuth } from "../hooks";
 import StudentDetailPage from "./StudentDetailPage";
 import PortalStudentDetailPage from "./PortalStudentDetailPage";
 import StudentSiblingsPanel from "../components/students/StudentSiblingsPanel";
+import StudentFeeManagementPanel from "../components/fees/StudentFeeManagementPanel";
 import { useParams } from "react-router-dom";
 
 const PORTAL_ROLES = new Set(["teacher", "student", "parent"]);
@@ -14,6 +15,7 @@ export default function RoleAwareStudentDetailPage() {
     <div className="space-y-5">
       <StudentDetailPage />
       <StudentSiblingsPanel studentId={id} editable />
+      <StudentFeeManagementPanel studentId={id} editable />
     </div>
   );
 }
